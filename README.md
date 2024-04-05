ac7ee9186595ab7749301f4fe418cd5c4c965499
Lightbender255 <lightbender255@gmail.com>
2024-04-05 10:02:59 -0600

    Started thumbnail script.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lightbender's Blender3D Scripts
+
+## Scripts
+
+./src/blender_gltf_thumbnail_producer.py
--- src/blender_gltf_thumbnail_producer.py
+++ src/blender_gltf_thumbnail_producer.py
@@ -1,1 +1,13 @@
+import bpy
+import math
 
+# Create as camera if one doesn't exist
+
+if not bpy.data.objects.get('Camera'):
+    bpy.ops.object.camera_add(location=(0,0,10))
+    camera = bpy.context.object
+else:
+    # Use the existing camera
+    camera = bpy.data.objects['Camera']
+    
+# Set the camera properties for a 2d-style setup
